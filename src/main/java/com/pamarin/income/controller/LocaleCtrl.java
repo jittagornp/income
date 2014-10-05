a922a7d7efa82baa01048f5f1cfeb099299860ca
jittagorn pitakmetagoon <jittagornp@gmail.com>
2014-10-05 22:51:32 +0700

    display currency with income value

--- src/main/java/com/pamarin/income/controller/LocaleCtrl.java
+++ src/main/java/com/pamarin/income/controller/LocaleCtrl.java
@@ -5,7 +5,10 @@
  */
 package com.pamarin.income.controller;
 
+import com.pamarin.income.model.Settings;
+import com.pamarin.income.security.SecurityUtils;
 import java.io.Serializable;
+import java.util.Currency;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -17,11 +20,24 @@ import org.springframework.stereotype.Component;
 @Scope("session")
 public class LocaleCtrl implements Serializable {
 
+    private Currency currency;
+
+    public void reset() {
+        if (!SecurityUtils.isAnonymous()) {
+            Settings settings = SecurityUtils.getUser().getSettings();
+            currency = Currency.getInstance(settings.getCurrencyCode());
+        }
+    }
+
     public String getCalendarFormat() {
         return "dd/MM/yyyy";
     }
 
     public String getCalendarTimeFormat() {
         return "dd/MM/yyyy HH:mm:ss น.";
+    }
+
+    public Currency getCurrency() {
+        return currency;
     }
 }
