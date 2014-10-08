24d442385e6cfe1454e5a0268574d79e4c32b728
jittagorn pitakmetagoon <jittagornp@gmail.com>
2014-10-09 01:04:50 +0700

    check first date of week

---
+++ src/test/java/com/pamarin/income/util/JodaTimeT.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pamarin.income.util;
+
+import org.joda.time.DateTimeConstants;
+import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author anonymous
+ */
+public class JodaTimeT {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JodaTimeT.class);
+    
+    @Test
+    public void firstDateOfThisWeek() {
+        LocalDate now = new LocalDate();
+        LOG.debug("first date of week --> {}", now.withDayOfWeek(DateTimeConstants.MONDAY));
+        LOG.debug("last date of week --> {}", now.withDayOfWeek(DateTimeConstants.SUNDAY)); 
+    }
+}
